dbd2b3141e30b55241527eab5cb71c7ca161c87c
Jim Collins <jim.r.collins@gmail.com>
2015-08-11 21:32:15 +0100

    Forgot to commit tests

--- src/test/scala/com/hmrc/shoppingcart/CheckoutUnitSpec.scala
+++ src/test/scala/com/hmrc/shoppingcart/CheckoutUnitSpec.scala
@@ -6,5 +6,22 @@ import org.specs2.mutable._
  * Created by Jim on 10/08/2015.
  */
 class CheckoutUnitSpec extends Specification {
-
+  "A checkout" should {
+    """correctly total a mixed basket of apples and oranges""" in {
+      val checkout = new Checkout{}
+      checkout.checkout(List(Apple,Apple,Orange,Apple)) must  beEqualTo(2.05)
+    }
+    """not charge for an empty basket""" in {
+      val checkout = new Checkout{}
+      checkout.checkout(List()) must beEqualTo(0)
+    }
+    """charge correct amount for 1 apple""" in {
+      val checkout = new Checkout{}
+      checkout.checkout(List(Apple)) must beEqualTo(0.60)
+    }
+    """charge correct amount for 1 orange""" in {
+      val checkout = new Checkout{}
+      checkout.checkout(List(Orange)) must beEqualTo(0.25)
+    }
+  }
 }
